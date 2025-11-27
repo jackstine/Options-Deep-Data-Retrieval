b7931d5e8f5a4d8d239fbde309f9c596e0889957
Jake Cukjati <jakecukjati@gmail.com>
2025-11-27 17:17:55 -0600

    adding in nasdaq enum

--- src/database/equities/enums.py
+++ src/database/equities/enums.py
@@ -7,6 +7,7 @@ class DataSourceEnum(str, Enum):
     """Enum for data source providers."""
 
     EODHD = "EODHD"
+    NASDAQ_SCREENER = "NASDAQ_SCREENER"
 
     def __str__(self) -> str:
         """Return the string value of the enum."""
